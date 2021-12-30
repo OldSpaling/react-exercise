5fd74b617340042d3e7de8570a08a6873ee256fd
xnguo <xiaoning.guo@innocellence.com>
2021-12-30 11:28:00 +0800

    feat:timer by state and lifetime hook

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const name = {
-  firstName: "josh", lastName: ' Perez'
-};
-const element = <h1>hello,{name.firstName + name.lastName}</h1>;
-const element1 = (<h1>hello,{name.firstName + name.lastName}</h1>);
-/**
- * @name 名字必须使用大驼峰的命名
- * @description  <Element2 firstName={name.firstName} lastName={name.lastName}></Element2>
- * @param {*} props 
- */
-function Element2(props) {
-  return <h1>hello,{props.firstName + props.lastName}</h1>;
-}
-/**
- * @name 名字必须使用大驼峰的命名
- * @description  <Element3 firstName={name.firstName} lastName={name.lastName}></Element3>
- * @param {*} props
- */
-class Element3 extends React.Component {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date };
+  }
+  /**
+   * 组件第一次被渲染到Dom时候触发
+   */
+  componentDidMount(){
+    this.timeID=setInterval(()=>this.tick(),1000);
+  }
+  /**
+   * 组件从dom中移除时候触发
+   */
+  componentWillUnmount(){
+    clearInterval(this.timeID);
+  }
+  tick(){
+    this.setState({time:new Date})
+  }
   render() {
-    return (<h1>hello,{this.props.firstName + this.props.lastName}</h1>);
+    return <div>{this.state.time.toLocaleTimeString()}</div>
   }
 }
 ReactDom.render(
-  <Element2 firstName={name.firstName} lastName={name.lastName}></Element2>,
+  <Clock/>,
   document.getElementById('root'));
