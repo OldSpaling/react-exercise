dc25cc8903fcebb731e50d305dccd35dec2b9022
guoxiaoning92125 <guoxiaoning92125@126.com>
2022-01-13 22:52:17 +0800

    feat: active link by style

--- my-app/src/components/invoices.jsx
+++ my-app/src/components/invoices.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { getInvoices } from '../data';
 export default class Invoices extends React.Component {
+    constructor(props) {
+        super(props);
+        this.activeLinkByStyle = this.activeLinkByStyle.bind(this);
+    }
+    activeLinkByStyle({ isActive }) {
+        return {
+            display: "block",
+            margin: "1rem 0",
+            color: isActive ? "red" : ""
+        };
+    }
     render() {
         const invoices = getInvoices();
         return (
             <div style={{ display: "flex" }}>
                 <nav style={{
                     borderRight: "solid 1px",
-                    padding:"1rem"
+                    padding: "1rem"
                 }}>
                     {
                         invoices.map(invoice => (
-                            <Link style={{ display: "block", margin: "1rem 0" }} to={`/invoices/${invoice.number}`}key={invoice.number}>
+                            <NavLink style={this.activeLinkByStyle} to={`/invoices/${invoice.number}`} key={invoice.number}>
                                 {invoice.name}
-                            </Link>
+                            </NavLink>
                         ))
                     }
                 </nav>
-                <Outlet/>
+                <Outlet />
             </div>
         );
     }
