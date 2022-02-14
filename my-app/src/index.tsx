cbc0f3bfcf89ca40ee204a2f4de45a66c838e31e
IC\xiaoning.guo <xiaoning.guo@innocellence.com>
2022-02-14 13:29:31 +0800

    feat:code init

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,11 +1,21 @@
-import reactDom from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import App from './app.component';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-//The "*" has special meaning here. It will match only when no other routes do.
-
-reactDom.render(
-   <BrowserRouter >
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-   </BrowserRouter>,
-   document.getElementById('root'));
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- my-app/src/pages/message.component.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class MessageComponent extends React.Component {
-    render() {
-        return <h3>Message {this.props.params.id}</h3>
-    }
-}
