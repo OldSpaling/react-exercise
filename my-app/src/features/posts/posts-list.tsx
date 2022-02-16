621e4e155709bf082584b62392746e102a471e55
IC\xiaoning.guo <xiaoning.guo@innocellence.com>
2022-02-16 11:11:07 +0800

    feat:sample demo

--- my-app/src/features/posts/posts-list.tsx
+++ my-app/src/features/posts/posts-list.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { RootState, useAppSelector } from '../../app/store';
 import './posts-list.module.css';
-export const PostList=()=>{
-    const posts=useAppSelector((state:RootState)=>{
+export const PostList = () => {
+    const posts = useAppSelector((state: RootState) => {
         return state.posts;
     });
-    const renderedPosts=posts.map((post)=>{
-        return <article className='post-excerpt'key={post.id}>
+    const renderedPosts = posts.map((post) => {
+        return <article className='post-excerpt' key={post.id}>
             <h3>{post.title}</h3>
-            <p className='post-content'>{post.content.substring(0,100)}</p>
+            <p className='post-content'>{post.content.substring(0, 100)}</p>
         </article>
     });
-    return(
+    return (
         <section className='posts-list'>
-            <h2>Posts</h2>
+            <h2>帖子列表</h2>
             <div className="actions">
                 <NavLink to="/list/add">Add</NavLink>
             </div>
